33116aab4a970d0c0e1f48fe40a9aadf5ac001fe
KimSongMok <mok0618@naver.com>
2024-02-21 20:03:19 +0900

    error solve

---
+++ src/main/java/hello/hellospring/controller/MemberForm.java
@@ -0,0 +1,16 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberForm {
+  private String name;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
