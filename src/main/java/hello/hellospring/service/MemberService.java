ba4cb931b9498b67caeca2e67da89751e899df12
KimSongMok <mok0618@naver.com>
2024-03-01 21:51:34 +0900

    Spring data JPA

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -40,6 +40,7 @@ public class MemberService {
     memberRepository.findByName(member.getName())
             .ifPresent(m -> {
               throw new IllegalStateException("이미 존재하는 회원입니다.");
+
             });
   }
 
